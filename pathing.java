fa2fd6cf43ac7a98f80dfe0a521d60262cdfdc36
Ethan Thomson <69830615+Mast3rofducks@users.noreply.github.com>
2022-09-22 13:25:28 +1200

    Update and rename pathing.py to pathing.java

---
+++ pathing.java
@@ -0,0 +1,6 @@
+// ideas:  
+* direct route: best speedwise, problems are many, the other bot, other bricks. https://csis.pace.edu/~benjamin/teaching/cs627/webfiles/Astar.pdf
+* starcraft 1: slower then direct, can track other bot and bricks, because the bot go's along predefined paths its less likely to fuck up, 
+* problem of needing 2 grids or useing direct rout once the bot is in its square.  https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7063238 http://striketactics.net/devblog/starcraft-1-pathfinding-technical-analysis
+* 
+//
