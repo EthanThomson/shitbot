70da137f4db3959a0596a2227d4bef57c3291601
mast3rofducks <mast3rofducks@gmail.com>
2022-09-26 19:47:36 +1300

    yeye

--- mov.py
+++ mov.py
@@ -1,11 +1,26 @@
 #imps
 from tkinter import *
+
+
 root = Tk()
 
-#import RPi.GPIO as pin
-#pin.setmode(pin.BCM)
+from gpiozero import PhaseEnableMotor, Pin, BadPinFactory, PinFactoryFallback, exc
 
 #bing chilling
+leftmotor = PhaseEnableMotor(12, 5)
+rightmotor = PhaseEnableMotor(13, 6)
+def goforwards():
+    leftmotor.forward()
+    rightmotor.forward()
+    logbox.insert(END, "forwards: ")
+    logbox.update()
+
+def gobackwards():
+    leftmotor.backward()
+    rightmotor.backward()
+    logbox.insert(END, "backwards: ")
+    logbox.update()
+
 
 #gui
 ##:sliders feed into profile
@@ -13,7 +28,12 @@ root = Tk()
 root.title('Control Board V1')
 
 
-#labels&frames
+#labels&frames&fields
+lmpha = Entry(root, width= 10)
+lmen = Entry(root, width= 10)
+rmpha = Entry(root, width= 10)
+rmen = Entry(root, width= 10)
+
 
 bothead = Label(root, text="Bot:")
 slidershead = Label(root, text="Sliders:")
@@ -35,27 +55,37 @@ terminalscroll.config(command=terminalbox.yview)
 #sliders
 glodalspeed = Scale(root, from_= 1, to= 100, orient= HORIZONTAL)
 
-
-
+wkey = Button(root, text= "w",command= goforwards)
+skey = Button(root, text="s", command= gobackwards)
 #griding
 bothead.grid(row=0, column=0)
 slidershead.grid(row=0, column=3)
-terminalhead.grid(row=1, column=0)
+terminalhead.grid(row=3, column=0)
 loghead.grid(row=1, column=3)
-logbox.grid(row=3, column=3)
-logscroll.grid(row=3, column=4)
-terminalbox.grid(row= 3, column=0)
-terminalscroll.grid(row= 3, column=1)
+logbox.grid(row=4, column=3)
+logscroll.grid(row=4, column=4)
+terminalbox.grid(row= 4, column=0)
+terminalscroll.grid(row= 4, column=1)
+
+wkey.grid(row= 1, column= 0)
+skey.grid(row= 2, column= 0)
 
 glodalspeed.grid(row= 1, column=3)
+lmpha.grid(row=2, column=3)
+lmen.grid(row=2, column=4)
+rmpha.grid(row=3, column=3)
+rmen.grid(row=3, column=4)
 
-#exec
 root.mainloop()
-
 
 
 #router
 ##:router takes profile as consts and then uses "wasd" to move the bot  might make diagnal keys aswell
+#leftmotor = PhaseEnableMotor(lmpha.get(), lmen.get())
+#rightmotor = PhaseEnableMotor(rmpha.get(), rmen.get())
+
+
+
 
 
 
